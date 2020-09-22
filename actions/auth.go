5c1cb1a963c106add621674e9cc3500924451a02
soypat <graded.sp@gmail.com>
2020-09-21 21:53:18 -0300

    rm profile scope for fb

--- actions/auth.go
+++ actions/auth.go
@@ -22,7 +22,7 @@ func init() {
 		google.New(os.Getenv("GGL_KEY_FORUM"), os.Getenv("GGL_SECRET_FORUM"), fmt.Sprintf("%s%s", App().Host, "/auth/google/callback"),
 			"profile", "email"),
 		facebook.New(os.Getenv("FB_KEY_FORUM"), os.Getenv("FB_SECRET_FORUM"), fmt.Sprintf("%s%s", App().Host, "/auth/facebook/callback"),
-			"profile", "email"),
+			"user_likes", "email"),
 	)
 }
 
